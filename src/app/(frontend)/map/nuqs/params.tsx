76e53d35c2a45607c6be8e508dfbc89c38307df2
Dan Sivewright <dan@wixels.com>
2025-07-15 15:37:45 +0200

    Lekka clustering

--- src/app/(frontend)/map/nuqs/params.tsx
+++ src/app/(frontend)/map/nuqs/params.tsx
@@ -1,6 +1,9 @@
-import { createSearchParamsCache, parseAsArrayOf, parseAsString } from 'nuqs/server'
+import { createSearchParamsCache, parseAsArrayOf, parseAsFloat, parseAsString } from 'nuqs/server'
 export const mapSearchParams = {
   markers: parseAsArrayOf(parseAsString).withDefault([]),
   polygons: parseAsArrayOf(parseAsString).withDefault([]),
+  longitude: parseAsFloat.withDefault(24.7499),
+  latitude: parseAsFloat.withDefault(-28.7282),
+  zoom: parseAsFloat.withDefault(5),
 }
 export const loadMapSearchParams = createSearchParamsCache(mapSearchParams)
